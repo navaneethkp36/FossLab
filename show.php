7f3bf05222587376a04ef06262d4431f780935c6
BSNeeraja <37290778+BSNeeraja@users.noreply.github.com>
2018-04-09 11:23:56 +0530

    Add files via upload
    required files

---
+++ show.php
@@ -0,0 +1,48 @@
+<HTML>
+<body background="./bg.jpg" text=#FFFFFF><div>
+<?php
+$name=$_POST['name'];
+
+$link = mysqli_connect("localhost", "id5213884_root", "ABCD1234", "id5213884_gecskp");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt select query execution
+$sql = "SELECT * FROM user WHERE name='$name'" ;
+if($result = mysqli_query($link, $sql))
+{
+echo"<table border=2>";
+echo"<tr>";
+echo"<td>id</td>";
+echo"<td>name</td>";
+
+
+
+echo "</tr>";
+	
+	if(mysqli_num_rows($result)>0)
+		{
+			
+			
+	
+		while($row=mysqli_fetch_assoc($result)){
+		
+		
+			echo "<td>".$row['id']."</td>";
+			echo "<td>".$row['name']."</td>";
+			
+			
+			
+		}
+			
+			}
+		}
+
+?>
+</div>
+
+</body>
+</html>
---
+++ tablemanipulation.php
@@ -0,0 +1,24 @@
+<?php
+
+$link = mysqli_connect("localhost", "id5213884_root", "ABCD1234", "id5213884_gecskp");
+ 
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+$sql = "create table User(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name CHAR(30) NOT NULL,
+	mailid VARCHAR(40) NOT NULL,
+        password VARCHAR(8) NOT NULL,
+	
+    
+)";
+
+if(mysqli_query($link, $sql)){
+    echo "Table created successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+?>
